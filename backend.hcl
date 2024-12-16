3e2a1e52aec3d3a1305b8a2886da9a11e54d4a51
godfreyfm <gmaiwun@gmail.com>
2024-12-16 01:41:25 -0500

    Added backend.hcl for backend mconfig and also, the Readme.MD file

---
+++ backend.hcl
@@ -0,0 +1,4 @@
+bucket = "gmai360labs-terraform-state"
+region = "us-east-2"
+dynamodb_table = "gmai360labs_terraform_locks"
+encrypt = true
--- backend.tf
+++ backend.tf
@@ -2,10 +2,10 @@
 
 terraform {
   backend "s3" {
-    bucket = "gmai360labs-terraform-state"
+    # bucket = "gmai360labs-terraform-state"
     key = "global/s3/terraform.tfstate"
-    region = "us-east-2"
-    dynamodb_table = "gmai360labs_terraform_locks"
-    encrypt = true
+    # region = "us-east-2"
+    # dynamodb_table = "gmai360labs_terraform_locks"
+    # encrypt = true
   }
 }
